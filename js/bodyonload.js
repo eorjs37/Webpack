051301c355bcbddd36f88cb624db6a0d1e27548d
CHOI DAE GEON <chleorjs37@gmail.com>
2021-04-12 17:06:18 +0900

    배경화면 랜덤하게 설정

--- js/bodyonload.js
+++ js/bodyonload.js
@@ -1,10 +1,12 @@
 const body =document.querySelector('body');
 function init(){
+    const imageNumber = Math.floor((Math.random()*3)+1);
+
+    console.log(imageNumber);
     body.style.backgroundRepeat="no-repeat";
-    body.style.backgroundImage="url('./images/2.jpg')";
+    body.style.backgroundImage=`url(./images/${imageNumber}.jpg)`;
     body.style.backgroundSize="100%";
     body.style.opacity="0.6";
-    console.log(body);
 }
 
 init();
