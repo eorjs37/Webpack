b8f59962c72e830fac86460daf0d0e307fbd5f64
CHOI DAE GEON <chleorjs37@gmail.com>
2021-04-12 17:01:02 +0900

    메인화면 배경작업 document으로 배경화면작업

---
+++ js/bodyonload.js
@@ -0,0 +1,10 @@
+const body =document.querySelector('body');
+function init(){
+    body.style.backgroundRepeat="no-repeat";
+    body.style.backgroundImage="url('./images/2.jpg')";
+    body.style.backgroundSize="100%";
+    body.style.opacity="0.6";
+    console.log(body);
+}
+
+init();
