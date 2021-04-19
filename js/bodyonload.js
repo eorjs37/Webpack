c48e9336a26fcfa80c9d329b658588d12f6fd578
CHOI DAE GEON <chleorjs37@gmail.com>
2021-04-19 17:11:06 +0900

    크롬에서 시안 완료

--- js/bodyonload.js
+++ js/bodyonload.js
@@ -3,7 +3,8 @@ function init(){
     const imageNumber = Math.floor((Math.random()*3)+1);
 
     body.style.backgroundRepeat="no-repeat";
-    body.style.backgroundImage=`url(./images/${imageNumber}.jpg)`;
+    // body.style.backgroundImage=`url(./images/${imageNumber}.jpg)`;
+    body.style.backgroundImage='url(./images/' + imageNumber +'.jpg)';
     body.style.backgroundSize="cover";
     body.style.opacity="0.6";
     body.style.backgroundPosition="center";
