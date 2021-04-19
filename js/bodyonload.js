058113627da7a37b2d1f7343ca0624bfa830ac0e
CHOI DAE GEON <chleorjs37@gmail.com>
2021-04-19 16:45:53 +0900

    grid css 수정

--- js/bodyonload.js
+++ js/bodyonload.js
@@ -1,12 +1,12 @@
 const body =document.querySelector('body');
 function init(){
-    // const imageNumber = Math.floor((Math.random()*3)+1);
+    const imageNumber = Math.floor((Math.random()*3)+1);
 
-    // body.style.backgroundRepeat="no-repeat";
-    // // body.style.backgroundImage=`url(./images/${imageNumber}.jpg)`;
-    // body.style.backgroundSize="cover";
-    // body.style.opacity="0.6";
-    // body.style.backgroundPosition="center";
+    body.style.backgroundRepeat="no-repeat";
+    body.style.backgroundImage=`url(./images/${imageNumber}.jpg)`;
+    body.style.backgroundSize="cover";
+    body.style.opacity="0.6";
+    body.style.backgroundPosition="center";
 }
 
 init();
