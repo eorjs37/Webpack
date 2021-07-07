fe24e55a2b41cb564ade602a6cbfc4ab2d16d991
CHOI DAE GEON <chleorjs37@gmail.com>
2021-07-07 20:40:42 +0900

    babel 삭제 및 eslint 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,23 @@
 module.exports = {
-    "env": {
-        "browser": true, //브라우저
-        "node": true,  //req
-        "es2021": true //es12 최신버전 허용
-    },
-    "extends": ["eslint:recommended"], // 일단 eslint 추천하는 방식으로 할것.
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module",
-    },
-    "rules": {
-        "no-unexpected-multiline": "error",
-        "no-extra-semi": "error",
-    }
+  env: {
+    browser: true, //브라우저
+    node: true, //req
+    es2021: true, //es12 최신버전 허용
+  },
+  extends: ["eslint:recommended", "eslint-config-prettier"], // 일단 eslint 추천하는 방식으로 할것.
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {
+    "no-unexpected-multiline": "error",
+    "no-extra-semi": "error",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
 };
