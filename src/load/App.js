f0604a64328bc6d1b64d2cdd801e8b81768c291b
CHOI DAE GEON <chleorjs37@gmail.com>
2021-07-08 21:21:53 +0900

    eslint로 인한 코드 정렬 및 수정

--- src/load/App.js
+++ src/load/App.js
@@ -1,29 +1,26 @@
-import { init } from './Init' 
-import { MenuBar} from './MenuBar';
-import 'ImagePath/icon/computer.svg';
-import 'ImagePath/icon/github.svg';
-import 'ImagePath/icon/phone.svg';
-import 'ImagePath/icon/gmail.svg';
-import 'ImagePath/icon/left-arrow.svg';
+import { init } from "./Init";
+import { MenuBar } from "./MenuBar";
+import "ImagePath/icon/computer.svg";
+import "ImagePath/icon/github.svg";
+import "ImagePath/icon/phone.svg";
+import "ImagePath/icon/gmail.svg";
+import "ImagePath/icon/left-arrow.svg";
 
+export class App {
+  constructor() {
+    this.pInit = new init();
+  }
 
-export class App{
-    constructor(pInit){
-        this.pInit = new init();
-    }
+  /* 최초 로드 */
+  appLoad() {
+    const imgNum = Math.floor(Math.random() * 3 + 1);
+    const main = document.querySelector("main.main-container");
 
-    /* 최초 로드 */
-    appLoad(){
-        const imgNum = Math.floor((Math.random()*3)+1);
-        const main = document.querySelector('main.main-container');
+    const Init = new init(imgNum, main);
+    new MenuBar();
+    Init.setBackGroundImg();
 
-        const Init = new init(imgNum,main);
-        const menuBar = new MenuBar();
-        Init.setBackGroundImg();
-
-        /* 날씨정보 초기화  */
-        Init.setCityWeater();
-
-    }
+    /* 날씨정보 초기화  */
+    Init.setCityWeater();
+  }
 }
-
--- src/load/Init.js
+++ src/load/Init.js
@@ -1,21 +1,20 @@
-import 'ImagePath/1.jpg';
-import 'ImagePath/2.jpg';
-import 'ImagePath/3.jpg';
-import {Weather} from 'Load/Weather';
+import "ImagePath/1.jpg";
+import "ImagePath/2.jpg";
+import "ImagePath/3.jpg";
+import { Weather } from "Load/Weather";
 
-export class init{ 
-    constructor(imageNumber,main){
-        this.imageNumber = Math.floor((Math.random()*3)+1);
-        this.main  = document.querySelector('main.main-container');
-        this.cityWeater  = new Weather();
-    }
+export class init {
+  constructor() {
+    this.imageNumber = Math.floor(Math.random() * 3 + 1);
+    this.main = document.querySelector("main.main-container");
+    this.cityWeater = new Weather();
+  }
 
-    setBackGroundImg(){
-        this.main.style.backgroundImage=`url(./assets/${this.imageNumber}.jpg)`;
-    }
+  setBackGroundImg() {
+    this.main.style.backgroundImage = `url(./assets/${this.imageNumber}.jpg)`;
+  }
 
-    setCityWeater(){
-        this.cityWeater.cityWeater();
-    }
+  setCityWeater() {
+    this.cityWeater.cityWeater();
+  }
 }
-
