d4c541f9dd8e9cc6d252ae3c3205b4e7b7afceb3
CHOI DAE GEON <chleorjs37@gmail.com>
2021-07-12 09:42:36 +0900

    import "@" 추가

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import './css/index.css'
-import './css/header.css';
-import './css/card.css';
-import './css/grid.css';
-import './css/sidebar.css';
+import "@/css/index.css";
+import "@/css/header.css";
+import "@/css/card.css";
+import "@/css/grid.css";
+import "@/css/sidebar.css";
 
-import {App} from './load/App';
+import { App } from "./load/App";
 
 const app = new App();
 app.appLoad();
