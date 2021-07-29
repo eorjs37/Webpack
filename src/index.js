5d77c85f9cf658efcbed072ecb7056443cda4056
CHOI DAE GEON <chleorjs37@gmail.com>
2021-07-29 19:52:39 +0900

    weater.css 추가

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import "@/css/index.css";
 import "@/css/header.css";
 import "@/css/card.css";
-import "@/css/grid.css";
 import "@/css/sidebar.css";
-
-import { App } from "./load/App";
+import "@/css/weather.css";
+import { App } from "@/load/App";
 
 const app = new App();
 app.appLoad();
